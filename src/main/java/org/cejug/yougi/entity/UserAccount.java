18d400ac222587fffd8d9dddf8203eb7dec9c14d
Alexandro <jornalsinal@gmail.com>
2014-04-17 14:10:44 -0300

    changes unchecked warnings new changes ok

--- src/main/java/org/cejug/yougi/entity/UserAccount.java
+++ src/main/java/org/cejug/yougi/entity/UserAccount.java
@@ -23,9 +23,7 @@ package org.cejug.yougi.entity;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
-
 import javax.persistence.*;
-
 import org.cejug.yougi.util.TextUtils;
 
 /**
--- src/main/java/org/cejug/yougi/knowledge/web/controller/UnpublishedArticlesMBean.java
+++ src/main/java/org/cejug/yougi/knowledge/web/controller/UnpublishedArticlesMBean.java
@@ -25,7 +25,6 @@ import org.cejug.yougi.knowledge.entity.WebSource;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
 import java.util.ArrayList;
 import java.util.List;
 
