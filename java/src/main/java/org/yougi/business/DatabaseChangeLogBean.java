1559600612b79d76e98a903b217eec0df397d873
Hildeberto Mendonca <me@hildeberto.com>
2014-07-16 23:42:15 +0200

    Fixed #252: Visualisation of database change logs finished.

--- java/src/main/java/org/yougi/business/DatabaseChangeLogBean.java
+++ java/src/main/java/org/yougi/business/DatabaseChangeLogBean.java
@@ -20,31 +20,15 @@
  * */
 package org.yougi.business;
 
-import org.jboss.vfs.TempFileProvider;
-import org.jboss.vfs.VFS;
-import org.jboss.vfs.VirtualFile;
 import org.yougi.entity.DatabaseChangeLog;
-import org.yougi.exception.EnvironmentResourceException;
 import org.yougi.util.PackageResourceHelper;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.io.BufferedReader;
-import java.io.Closeable;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -56,7 +40,6 @@ import java.util.logging.Logger;
 public class DatabaseChangeLogBean extends AbstractBean<DatabaseChangeLog> {
 
     private static final Logger LOGGER = Logger.getLogger(DatabaseChangeLogBean.class.getSimpleName());
-    private static final String CHANGELOG_PATH = "org/yougi/db/changelog";
 
     @PersistenceContext
     private EntityManager em;
@@ -99,7 +82,7 @@ public class DatabaseChangeLogBean extends AbstractBean<DatabaseChangeLog> {
     }
 
     private File getChangeLogFile(String id) {
-        List<File> files = PackageResourceHelper.INSTANCE.getFilesFolder(CHANGELOG_PATH);
+        List<File> files = PackageResourceHelper.INSTANCE.getFilesFolder("org/yougi/db/changelog");
         String filename;
         for(File file : files) {
             if (file.getName().endsWith(".sql")) {
@@ -111,47 +94,5 @@ public class DatabaseChangeLogBean extends AbstractBean<DatabaseChangeLog> {
             }
         }
         return null;
-    }
-
-    private Set<String> getChangeLogFiles() {
-        final ClassLoader loader = JobSchedulerBean.class.getClassLoader();
-        URL url = loader.getResource(CHANGELOG_PATH);
-
-        if (url == null) {
-            return Collections.emptySet();
-        }
-
-        VirtualFile virtualFile;
-        Closeable handle = null;
-        String protocol = url.getProtocol();
-        Set<String> names = new HashSet<>();
-        try {
-            if ("vfs".equals(protocol)) {
-                URLConnection conn = url.openConnection();
-                virtualFile = (VirtualFile) conn.getContent();
-            } else if ("file".equals(protocol)) {
-                virtualFile = VFS.getChild(url.toURI());
-                File archiveFile = virtualFile.getPhysicalFile();
-                TempFileProvider provider = TempFileProvider.create("tmp", Executors.newScheduledThreadPool(2));
-                handle = VFS.mountZip(archiveFile, virtualFile, provider);
-            } else {
-                throw new UnsupportedOperationException("Protocol " + protocol + " is not supported");
-            }
-
-            List<VirtualFile> files = virtualFile.getChildren();
-            for(VirtualFile ccFile : files) {
-                if (ccFile.getName().endsWith(".sql")) {
-                    names.add(ccFile.getName());
-                }
-            }
-
-            if(handle != null) {
-                handle.close();
-            }
-        } catch (IOException | URISyntaxException ioe) {
-            throw new EnvironmentResourceException(ioe.getMessage(), ioe);
-        }
-
-        return names;
     }
 }
