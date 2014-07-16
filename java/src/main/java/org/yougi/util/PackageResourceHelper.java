15439e75f6f091d0cbd6483a84e3e9bfee022f5e
Hildeberto Mendonca <me@hildeberto.com>
2014-07-16 23:46:01 +0200

    Fixed #252: Visualisation of database change logs finished.

--- java/src/main/java/org/yougi/util/PackageResourceHelper.java
+++ java/src/main/java/org/yougi/util/PackageResourceHelper.java
@@ -1,3 +1,23 @@
+/* Yougi is a web application conceived to manage user groups or
+ * communities focused on a certain domain of knowledge, whose members are
+ * constantly sharing information and participating in social and educational
+ * events. Copyright (C) 2011 Hildeberto Mendonça.
+ *
+ * This application is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This application is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * There is a full copy of the GNU Lesser General Public License along with
+ * this library. Look for the file license.txt at the root level. If you do not
+ * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA.
+ * */
 package org.yougi.util;
 
 import org.jboss.vfs.TempFileProvider;
@@ -18,7 +38,9 @@ import java.util.List;
 import java.util.concurrent.Executors;
 
 /**
- * Created by mendoncafilh on 16/07/2014.
+ * Encapsulates the complexity of getting resources from the war file.
+ *
+ * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 public enum PackageResourceHelper {
 
